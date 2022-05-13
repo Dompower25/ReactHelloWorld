6b4f0faf97c60a053e801155adb96b58b0fc30b4
Evgeniy Domanovich <dompro1994.ed@gmail.com>
2022-05-13 14:22:00 +0300

    fixed function addNewPost (createPost) in PostFomr

--- my-app/src/components/PostForm.jsx
+++ my-app/src/components/PostForm.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import MyInput from './UI/input/MyInput';
 import MyButton from './UI/button/MyButton';
 
-function PostForm({ create }) {
+function PostForm({create}) {
   const [post, setPost] = useState({ title: "", body: "" });
 
   const addNewPost = (e) => {
@@ -11,22 +11,22 @@ function PostForm({ create }) {
       ...post,
       id: Date.now(),
     };
-    create={newPost}
-    setPost([...post, newPost]);
+    create(newPost)
+    setPost({title: '', body: ''})
   };
 
   return (
     <form>
       <MyInput
         value={post.title}
-        onChange={(event) => setPost(event.target.value)}
+        onChange={(event) => setPost({ ...post, title: event.target.value })}
         type="text"
         placeholder="Название поста"
       />
 
       <MyInput
         value={post.body}
-        onChange={(event) => setPost(event.target.value)}
+        onChange={(event) => setPost({ ...post, body: event.target.value })}
         type="text"
         placeholder="Описание поста"
       />
